d838063513bcb02d260b007ec94b967823106d61
kyi-Thar-Soe <kyis8571@gmail.com>
2025-01-06 21:53:20 +0630

    fixed txt

--- src/Pages/AboutSection/AboutSection.jsx
+++ src/Pages/AboutSection/AboutSection.jsx
@@ -1,22 +1,46 @@
-import './AboutSection.css';
+import "./AboutSection.css";
 
 export default function AboutSection() {
-    return (
-        <div className="home-parent-container">
-            <div className="home-sub-container">
-            <div className="about-container" id="about">
-            <div className='about-info'>
+  return (
+    <div className="home-parent-container">
+      <div className="home-sub-container">
+        <div className="about-container" id="about">
+          <div className="about-info">
             <h6>About</h6>
             <h3>About Me</h3>
-            <div className='para'>
-            <p>Hello, my name is Kyi Thar Soe. I'm 23 years old. My education background is that I studied IT major at Mawlamyine University of Technology.</p>
-            <p>I have 2 years experience in web development field. And, I have 1 year of experience working as a front-end web developer at a japanese company, a foreign company. I have a deep understanding of HTML, CSS, JavaScript, JQuery and Wordpress, and I'm skilled in various modern frameworks and libraries like React, Bootstrap and Tailwind CSS. And, I'm familiar with Restfuls API and Git in version control system. I have basic knowledge in Node.js. In addition, I know about Design System and UX Research related to aspects of Web Design. Then, I have certificates and achievement related to Web Development, React and UI/UX Web Design. I have done projects such as E-commerce, Admin Dashboard, Movie App, Education Platform and Real World projects.As for the Education Platform, I worked together with a team. And, we won the best prize with this project.</p>
-            <p>Beyond code, I'm a firm believer in the importance of collaboration and communication. I'm excited to tackle new challenges, bring fresh ideas to the table, and create intuitive web experiences that leave a lasting impression.</p>
-            {/* <p> I thrive in interdisciplinary teams and enjoy translating complex technical concepts into layman's terms, fostering effective project management and problem-solving. When I'm not immersed in code, you can find me experimenting with new design tools, attending tech meetups, or contributing to open-source projects.</p> */}
+            <div className="para">
+              <p>
+                Hello,my name is Kyi Thar Soe.I'm 23 years old.My education
+                background is that I studied IT major at Mawlamyine University
+                of Technology.
+              </p>
+              <p>
+                I have 2 years experience in web development field.And,I have 1
+                year of experience working as a front-end web developer at a
+                japanese company,a foreign company.I have a deep understanding
+                of HTML,CSS,JavaScript,JQuery and Wordpress,and I'm skilled in
+                various modern frameworks and libraries like React,Bootstrap and
+                Tailwind CSS.And,I'm familiar with Restfuls API and Git in
+                version control system.I have basic knowledge in Node.js.In
+                addition,I know about Design System and UX Research related to
+                aspects of Web Design.Then,I have certificates and achievement
+                related to Web Development,React and UI/UX Web Design.I have
+                done projects such as E-commerce,Admin Dashboard,Movie App,
+                Education Platform and Real World projects.As for the Education
+                Platform,I worked together with a team.And,we won the best prize
+                with this project.
+              </p>
+              <p>
+                Beyond code,I'm a firm believer in the importance of
+                collaboration and communication. I'm excited to tackle new
+                challenges,bring fresh ideas to the table,and create intuitive
+                web experiences that leave a lasting impression.
+              </p>
+              {/* <p> I thrive in interdisciplinary teams and enjoy translating complex technical concepts into layman's terms, fostering effective project management and problem-solving. When I'm not immersed in code, you can find me experimenting with new design tools, attending tech meetups, or contributing to open-source projects.</p> */}
             </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
