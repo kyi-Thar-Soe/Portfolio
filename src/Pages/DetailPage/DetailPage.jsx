b4d6198779f0fb1e8d910e4bf284d6f414ccacca
kyi-Thar-Soe <kyis8571@gmail.com>
2025-01-06 23:06:09 +0630

    fixed detail

--- src/Pages/DetailPage/DetailPage.jsx
+++ src/Pages/DetailPage/DetailPage.jsx
@@ -1,59 +1,65 @@
-import './DetailPage.css';
+import "./DetailPage.css";
 import { useNavigate, useParams } from "react-router";
 import { cards } from "../../Data/CardData";
 import { useEffect, useState } from "react";
-import KeyboardBackspaceRoundedIcon from '@mui/icons-material/KeyboardBackspaceRounded';
+import KeyboardBackspaceRoundedIcon from "@mui/icons-material/KeyboardBackspaceRounded";
 export default function DetailPage() {
-    const [project, setProject] = useState(null);
-    const {id} = useParams();
-    const navigate = useNavigate();
+  const [project, setProject] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const fetchData = async () => {
-        try {
-           const localData = cards;
-            const selectedItem = localData.find((item) => item.id === parseInt(id));
-            setProject(selectedItem);
-            } catch (error) {
-            console.error('Error fetching data:', error);
-            }
-        };
+  const fetchData = async () => {
+    try {
+      const localData = cards;
+      const selectedItem = localData.find((item) => item.id === parseInt(id));
+      setProject(selectedItem);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [id]);
-    return (
-        <div>
-        {project && (
-            <div>
-                <div className='detailInfo-div'>
-                    <img src={project.backGround} alt='background-img'/>
-                <div className='detailsInfo-overlay'>
-                <span onClick={()=>navigate(-1)}><KeyboardBackspaceRoundedIcon/></span>
+  useEffect(() => {
+    fetchData();
+  }, [id]);
+  return (
+    <div>
+      {project && (
+        <div className="detail-container">
+          <div className="detailInfo-div">
+            <img src={project.backGround} alt="background-img" />
+            <div className="home-sub-container">
+              <div className="detailsInfo-overlay">
+                <span onClick={() => navigate(-1)}>
+                  <KeyboardBackspaceRoundedIcon />
+                </span>
                 <h1>{project.title}</h1>
                 <h4>{project.overview}</h4>
-                </div>
-                </div>
-                <div className="detailImage-div">
-                {/*Single image*/}
-                <div className='single-img'>
-                <h3>Screen 1</h3>
-                <img src={project.imageOne} alt="image"/>
-                </div>
-                <div className='single-img'>
-                <h3>Screen 2</h3>
-                <img src={project.imageTwo} alt="image"/>
-                </div>
-                <div className='single-img'>
-                <h3>Screen 3</h3>
-                <img src={project.imageThree} alt="image"/>
-                </div>
-                <div className='single-img'>
-                <h3>Screen 4</h3>
-                <img src={project.imageFour} alt="image"/>
-                </div>
-                </div>
+              </div>
             </div>
-        )}
+          </div>
+          <div className="home-sub-container">
+            <div className="detailImage-div">
+              {/*Single image*/}
+              <div className="single-img">
+                <h3>Screen 1</h3>
+                <img src={project.imageOne} alt="image" />
+              </div>
+              <div className="single-img">
+                <h3>Screen 2</h3>
+                <img src={project.imageTwo} alt="image" />
+              </div>
+              <div className="single-img">
+                <h3>Screen 3</h3>
+                <img src={project.imageThree} alt="image" />
+              </div>
+              <div className="single-img">
+                <h3>Screen 4</h3>
+                <img src={project.imageFour} alt="image" />
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
