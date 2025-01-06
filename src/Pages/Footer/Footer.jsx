987405a174c85141d15387f4942c3e6939d8308e
kyi-Thar-Soe <kyis8571@gmail.com>
2025-01-06 21:44:55 +0630

    added section

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -25,33 +25,43 @@ export default function Footer() {
     <footer className="footer-container" id="contactFoot">
       <div className="home-sub-container">
         <div className="footer-div">
-          {links.map((link) => (
-            <Button
-              href={link.path}
-              key={link.name}
-              className="footer-link-div"
-              onClick={() => handleFooterLinkClick(link.name)}
-              sx={{
-                my: 2,
-                display: "block",
-                textTransform: "capitalize",
-              }}
-            >
-              <span className="footer-link">{link.name}</span>
-            </Button>
-          ))}
+          <div className="footer-left-div">
+            {links.map((link) => (
+              <Button
+                href={link.path}
+                key={link.name}
+                className="footer-link-div"
+                onClick={() => handleFooterLinkClick(link.name)}
+                sx={{
+                  my: 2,
+                  display: "block",
+                  textTransform: "capitalize",
+                }}
+              >
+                <span className="footer-link">{link.name}</span>
+              </Button>
+            ))}
+          </div>
           <div className="contact-icon-div">
             <span className="ph-num">+95 9763460067</span>
             <span className="email-txt">kyis8571@gmail.com</span>
-            <a href="">
-              <img src={facebook} alt="facebook" />
-            </a>
-            <a href="">
-              <img src={linkedin} alt="linkedin" />
-            </a>
-            <a href="">
-              <img src={github} alt="github" />
-            </a>
+            <div className="social-icon">
+              <a
+                href="https://www.facebook.com/share/1A1FZ2y6oJ/"
+                target="_blank"
+              >
+                <img src={facebook} alt="facebook" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ma-kyi-thar-soe-043397292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                target="_blank"
+              >
+                <img src={linkedin} alt="linkedin" />
+              </a>
+              <a href="https://github.com/kyi-Thar-Soe" target="_blank">
+                <img src={github} alt="github" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/Pages/HomeSection/HomeSection.jsx
+++ src/Pages/HomeSection/HomeSection.jsx
@@ -1,30 +1,27 @@
-import './HomeSection.css';
-import profile from '../../assets/profile.jpg';
+import "./HomeSection.css";
+import profile from "../../assets/profile.jpg";
 import { Button } from "@mui/material";
 
 export default function HomeSection() {
-    return (
-        <div className="home-parent-container">
-            <div className="home-sub-container">
-            <div className="home-container"  id='home'>
-            <div className="introduce-div first-col-div">
-                <h1>Hello, I'm Kyi Thar Soe</h1>
-                <h2><span>front-End</span> web developer</h2>
-                <p>I am someone who is passionate  about creating websites and web applications that are desirable, feasible and viable.</p>
-                <Button variant="contained"
-                href='#contactFoot'
-                className='getInTouch-btn'
-                sx={{
-                    textTransform: 'capitalize',
-                    ':hover': {background: 'transparent',color: '#191919',border: '1px solid #191919',boxShadow: 'none'}}}>
-                <span className="getInTounch-txt">get in touch</span>
-                </Button>
-            </div>
-            <div className="img-div sec-col-div">
-              <img src={profile} alt='profile'/>
-            </div>
+  return (
+    <div className="home-parent-container">
+      <div className="home-sub-container">
+        <div className="home-container" id="home">
+          <div className="introduce-div first-col-div">
+            <h1>Hello, I'm Kyi Thar Soe</h1>
+            <h2>
+              <span>front-End</span> web developer
+            </h2>
+            <p>
+              I am someone who is passionate about creating websites and web
+              applications that are desirable, feasible and viable.
+            </p>
+          </div>
+          <div className="img-div sec-col-div">
+            <img src={profile} alt="profile" />
+          </div>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
